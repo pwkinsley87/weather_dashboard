8087ee4d2504469f3060c7acab32bb13fc1072de
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-05-21 19:33:04 -0500

    JSON data shows, but Search button does not show weather data when clicked. No CSS

--- script/script.js
+++ script/script.js
@@ -1,16 +1,103 @@
-// create a request variable and assign a new XMLHttpRequest object to it
+var languageButtonsEl = document.querySelector('#language-buttons');
+var citySearchFormEl = document.querySelector('#city-search-form');
 
-var city = document.querySelector('#city');
 
-var request= new XMLHttpRequest()
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+  
+    // get value from input element
+    var city = cityInputEl.value.trim();
+  
+    if (city) {
+      getCityData(city);
+  
+      // clear old content
+      cityContainerEl.textContent = '';
+      cityInputEl.value = '';
+    } else {
+      alert('Please enter a city');
+    }
+  };
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.openweathermap.org/data/', true)
+  var buttonClickHandler = function(event) {
+    // get the language attribute from the clicked element
+    var weather = event.target.getAttribute("current");
+  
+    if (weather) {
+      getCityData (weather);
+  
+      // clear old content
+      cityContainerEl.textContent = "";
+    }
+  };
 
-request.onload = function () {
-    //begin accessing JSON data here 
-    var data = JSON.parse(this.response)
-};
+  var getCityData = function(city) {
+    // format the github api url
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=134a917d802ddc10f402d9450d227bd4' + city + '/current';
+  
+    // make a get request to url
+    fetch(apiUrl)
+      .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function(data) {
+            console.log(data);
+            displayCity(data, city);
+          });
+        } else {
+          alert('Error: City not found');
+        }
+      })
+      .catch(function(error) {
+        alert('Unable to connect to weather. Is there any weather there? Maybe they have run out of weather.');
+      });
+  };
 
-// send request 
-request.send()
+  var displayCity = function(city, searchTerm) {
+    // check if api returned any repos
+    if (cities.length === 0) {
+      cityContainerEl.textContent = 'No data found.';
+      return;
+    }
+  
+    cityInputEl.textContent = searchTerm;
+  
+    // loop over repos
+    for (var i = 0; i < cities.length; i++) {
+      // format repo name
+      var cityName = cities[i].owner.login + '/' + cities[i].name;
+  
+      // create a container for each repo
+      var cityEl = document.createElement('div');
+      cityEl.classList = 'list-item flex-row justify-space-between align-center';
+  
+      // create a span element to hold repository name
+      var titleEl = document.createElement('span');
+      titleEl.textContent = cityName;
+  
+      // append to container
+      cityEl.appendChild(titleEl);
+  
+      // create a status element
+      var statusEl = document.createElement('span');
+      statusEl.classList = 'flex-row align-center';
+  
+      // append to container
+      cityEl.appendChild(statusEl);
+  
+      // append container to the dom
+      cityContainerEl.appendChild(cityEl);
+    }
+     
+var buttonClickHandler = (function(event) {
+    var language = event.target.getAttribute("data-language")
+    console.log(language)
+}
+  )};
+
+citySearchFormEl.addEventListener("click", buttonClickHandler);
+
+
+
