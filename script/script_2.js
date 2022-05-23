dd1311b5ebea57c8de75c26ab525e9ddf9d727d6
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-05-22 19:20:07 -0500

    further troubleshooting, error on line 8- button click function

--- script/script_2.js
+++ script/script_2.js
@@ -1,14 +1,24 @@
-var button = document.querySelector('.button');
-var inputValue = document.querySelector('.inputValue');
-var name = document.querySelector('.name');
-var name = document.querySelector('.description');
-var name = document.querySelector('.temp');
-var name = document.querySelector('.humidity');
+var button = document.querySelector('btn');
+var inputValue = document.querySelector('inputValue');
+var name = document.querySelector('name');
+var description = document.querySelector('description');
+var temp = document.querySelector('temp');
+var humidity = document.querySelector('humidity');
 
 button.addEventListener('click', function() {
-    fetch('api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=134a917d802ddc10f402d9450d227bd4') 
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue+'&appid=134a917d802ddc10f402d9450d227bd4') 
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        var nameValue = data['name'];
+        var temp = data['main']['temp'];
+        var descriptionValue = data['weather'][0]['description'];
+
+        cityName.innerHTML =nameValue;
+        temp.innerHTML = tempValue;
+        description.innerHTML = descriptionValue;
+    })
 
 .catch(err => alert("Bad input!"))
-})
+});
+
+
